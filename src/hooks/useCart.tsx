6ec9d32b852a04c2dba9da3577b280aea54c8240
Agathius <p.akos1148@gmail.com>
2023-09-20 23:51:15 +0200

    updated imports

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,6 +1,7 @@
 // custom hook to consume CartContext & export  
-import {useContext} from 'react'
-import CartContext, { UserCartContextType } from '../context/CartProvider' 
+import { useContext } from 'react'
+import CartContext from '../context/CartProvider' 
+import { UserCartContextType } from '../types/cartProviderTypes'
 
 const useCart = (): UserCartContextType => {
   return useContext(CartContext)
