9069cad707a578d6d32d4194e733d87d963103c6
Agathius <p.akos1148@gmail.com>
2023-09-22 23:49:28 +0200

    added responsive, styled header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,69 @@
 import React from 'react'
 import Nav from './Nav'
 import useCart from '../hooks/useCart'
+import { CartIcon } from './SVGComponents'
+import './Header.css'
+import SearchBar from './SearchBar'
 
 type PropsType = {
   viewCart: boolean,
   setViewCart: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
-
 const Header = ({viewCart, setViewCart}: PropsType) => {
+  const {totalItems} = useCart();
 
-  const {totalItems, totalPrice} = useCart();
+  // Elements
+  const title = (
+    <div className="header__title">
+      <h1> Acme co. </h1>
+    </div>
+  )
 
-  const content = (
-    <header className='header'> 
-      <div className="header__title-bar">
-        <h1> Acme Co.</h1>
-        <div className="header__price-box">
-          <p> Total Items: {totalItems} </p>
-          <p> Total Price: {totalPrice} </p>
-        </div>
-      </div> 
+  const navigation = (
+    <div className="header__nav">
       <Nav viewCart={viewCart} setViewCart={setViewCart}/>
+    </div>
+  )
+
+  const cart = (
+    <div className="cart-icon">
+      <CartIcon width='30px' height='30px' fill='var(--color-4)' wrapperCustomStyle={{'height': 'auto', 'width': 'auto','alignItems': 'center'}}/> 
+      {totalItems > 0 ? <span className="cart-icon__counter"> {totalItems > 100 ? '100+' : totalItems} </span> : null}
+    </div>
+  )
+
+  const searchBar = (
+    <div className="header__search-bar">
+      <SearchBar/>
+    </div>
+  )
+
+  // Layouts
+  const smallScreenLayout = (
+    <header className='header header__small-screen'>
+      {navigation}
+      {title}
+      {cart}
+      {searchBar}
     </header>
   )
 
-  return content
+  const largeScreenLayout = (
+    <header className='header header__large-screen'>
+      {title}
+      {searchBar}
+      {navigation}
+      {cart}
+    </header>
+  )
+
+  return (
+    <>
+      {smallScreenLayout}
+      {largeScreenLayout}
+    </>
+  )
 }
 
 export default Header
