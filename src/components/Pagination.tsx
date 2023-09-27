2506cd73db378907d6257414dfaecc66b831f6ff
Agathius <p.akos1148@gmail.com>
2023-09-27 23:54:07 +0200

    constructed working client side navigation with reusable btns

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,18 +1,78 @@
+// Component to handle pagination logic, and display paginated pages
 import './Pagination.css'
 
-const Pagination = () => {
+// TYPES
+type PaginationPropsType = {
+  activePage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+type NavigationButtonPropsType = {
+  key?: string | number
+  style?: string // added style through className  
+  clicked: () => void // click handler
+  disabled?: boolean // disabled state
+  label?: string | number // btn name
+}
+
+// COMPONENTS
+const Pagination = ({activePage, totalPages, onPageChange}: PaginationPropsType) => {
+
+  // CALCULATED & CONSTANT VALUES
+  // Navigation button various vonditions
+  const previousPageValue = activePage > 1 ? activePage - 1 : 1;
+  const previousPageDisabledValue = activePage === 1;
+  const nextPageValue = activePage < totalPages ? activePage + 1 : totalPages;
+  const nextPageDisabledValue = activePage === totalPages;
+  // Style
+  const navigationButtonStyle = 'button--pagination-navigation'; 
+  
+
+  // ELEMENTS
+  // Buttons
+  // go to a specific page 
+  const goToPageButton = Array.from({length: totalPages}, (_, i) => {
+    const pageValue = i + 1;
+    return (
+      <NavigationButton 
+        style={navigationButtonStyle} 
+        clicked={() => onPageChange(pageValue)} 
+        disabled={activePage === pageValue} 
+        label={pageValue}
+      />
+    )
+  })  
+
   return (
     <div className="pagination">
       <div className="pagination__navigation">
-        <button className='button--pagination-navigation'> {'previous'} </button>
-        <button className='button--pagination-navigation'> {'1'} </button>
-        <button className='button--pagination-navigation'> {'2'} </button>
-        <button className='button--pagination-navigation'> {'3'} </button>
-        <button className='button--pagination-navigation'> {'next'} </button>
+        <NavigationButton 
+          style={navigationButtonStyle}
+          clicked={() => {onPageChange(previousPageValue)}} 
+          disabled={previousPageDisabledValue} 
+          label='previous'
+        />
+        {goToPageButton} 
+        <NavigationButton 
+          style={navigationButtonStyle} 
+          clicked={() => {onPageChange(nextPageValue)}} 
+          disabled={nextPageDisabledValue} 
+          label='next'
+        />
       </div>
-      {/* <div className="pagination__page-selection">  </div> */}
     </div>
   )
 }
-
 export default Pagination
+
+// Reusable navigation btn used for page pagination 
+const NavigationButton = ({style, clicked, disabled, label}: NavigationButtonPropsType) => {
+  return (
+    <button 
+      className={style}
+      onClick={clicked}
+      disabled={disabled}
+    > {label} </button>
+  )
+}
