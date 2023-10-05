ad3e786d51a0645dcdaef80ff627994c0bc5fe15
Agathius <p.akos1148@gmail.com>
2023-10-05 18:07:52 +0200

    updated product page to handle categories, and search results, added routing and refined styling

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,25 +1,35 @@
 // Component to hold the product items and related pagination
-import {useEffect, useState, ReactElement} from 'react'
-import './ProductList.css'
+import {useEffect, ReactElement} from 'react'
+import './ProductPage.css'
 import {ProductItemType} from '../types/productsProviderTypes'
 import Pagination from './Pagination'
 import ProductList from './ProductList'
-import {useNavigate, useParams, useLocation} from 'react-router-dom'
+import {useNavigate, useLocation} from 'react-router-dom'
+import ProductSidemenu from './ProductSidemenu/ProductSidemenu'
+import useProducts from '../hooks/useProducts'
+
+const CONSTANT = {
+  HEADER_1_BROWSE_GOODIES: 'Browse Goodies',
+  HEADER_1_NO_PRODUCTS: 'No Available Products',
+  HEADER_2_NEW_PRODUCTS_SOON: 'New products are coming soon...'
+}
 
 // TYPE
 type PropsType = {
-  productsData: ProductItemType[] | undefined
+  productData: ProductItemType[] | undefined
 }
 
 // COMPONENT
-const ProductPage = ({productsData}: PropsType) => {
+const ProductPage = ({productData}: PropsType) => {
   // ROUTE
   const navigate = useNavigate();
-  const {page} = useParams();
+  // const {category, page} = useParams();
   const location = useLocation();
-  
-  // STATE
-  const [activePage, setActivePage] = useState<number>(parseInt(page || '1'));
+  const activeCategory = location.pathname.split('/').filter(segment => segment !== '').slice(0, -1).join('/');
+
+  // CONTEXT
+  const {activePage} = useProducts();
+  const pageNumber = activePage || 1
 
   // EFFECTS
   useEffect(() => {
@@ -27,44 +37,51 @@ const ProductPage = ({productsData}: PropsType) => {
   }, [])
 
   useEffect(() => {
-    const testLocation = location.pathname.split('').filter(segment => segment !== '').slice(0, -1).join('')
-    navigate(`${testLocation}${activePage}`, { replace: true });
-    console.log(`${testLocation}${activePage}`)
-  }, [activePage, location.pathname, navigate])
+    const activeRoute = activeCategory === 'search' ? '/search/1' : `/${activeCategory}/${pageNumber}`
+    navigate(activeRoute, { replace: true });
+  }, [activeCategory, pageNumber, navigate]);
 
-    // CONSTANT VALUES
-    const itemsPerPage = 10;
+  // Filter out productsData based on active category
+  const categoryProductData = productData?.filter(product => {
+    return product.category === activeCategory;
+  });
 
-    // CALCULATED VALUES
-    const displayedProductData: ProductItemType[] = productsData || []
-    const totalPages = Math.ceil(displayedProductData?.length / itemsPerPage);
-    const startIndex: number = (activePage - 1) * itemsPerPage;
-    const endIndex: number =  startIndex + itemsPerPage; 
+  const filteredProductData = activeCategory === 'all' || activeCategory === 'search'  ? productData : categoryProductData
 
-    const paginatedProducts: ProductItemType[] = productsData?.slice(startIndex, endIndex) || [];
-   
-  // HANDLERS
-  const setActivePageHandler = (page: number) => {
-    setActivePage(page)
-  }
+  // CONSTANT VALUES
+  const itemsPerPage = 10;
 
-  // ELEMENTS
-  // Loader
-  const loader: ReactElement | ReactElement[] = <p> Loading... </p>
+  // CALCULATED VALUES
+  const displayedProductData: ProductItemType[] = filteredProductData || []
+  const totalPages = Math.ceil(displayedProductData?.length / itemsPerPage);
+  const startIndex: number = (pageNumber - 1) * itemsPerPage;
+  const endIndex: number =  startIndex + itemsPerPage;
 
-  return ( 
-    <main className="main main--products">
-      <h1 className="main--products__title"> Browse goodies </h1>
-      { productsData?.length ? 
-        <ProductList productsData={paginatedProducts}/>
-        : 
-        loader 
-      }
-      <Pagination  
-        activePage={activePage}
-        totalPages={totalPages}
-        onPageChange={setActivePageHandler}
-      />
+  const paginatedProducts: ProductItemType[] = filteredProductData?.slice(startIndex, endIndex) || [];
+
+  // empty products list (fetch ok)
+  const productEmptyList: ReactElement | ReactElement[] = (
+    <div className="product-page__empty-list">
+      <h1 className='product-page__header--1'>{CONSTANT.HEADER_1_NO_PRODUCTS}</h1>
+      <h2 className='product-page__header--2'>{CONSTANT.HEADER_2_NEW_PRODUCTS_SOON}</h2>
+    </div>
+  )
+
+  // products list
+  const productList: ReactElement | ReactElement[] = (
+    <div className='product-page__content'>
+      <h1 className='product-page__header--1'>{CONSTANT.HEADER_1_BROWSE_GOODIES}</h1>
+      <ProductList productsData={paginatedProducts}/>
+      <Pagination totalPages={totalPages}/>
+    </div>
+  )
+
+  return (
+    <main className='main main__product-page'>
+      <div className="product-page__wrapper">
+        <ProductSidemenu activeCategory={activeCategory}/>
+        {filteredProductData?.length ? productList : productEmptyList}
+      </div>
     </main>
   )
 }
