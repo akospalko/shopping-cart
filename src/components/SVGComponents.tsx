c773b8ec90f7935122766e1cfbe3cd5c23da542f
Agathius <p.akos1148@gmail.com>
2023-10-13 23:57:32 +0200

    Added home icon

--- src/components/SVGComponents.tsx
+++ src/components/SVGComponents.tsx
@@ -182,7 +182,32 @@ export function ArrowIcon({width, height, fill, wrapperCustomStyle}: IconCompone
   );
 }
     
-
+// Home
+export function HomeIcon({width, height, stroke, wrapperCustomStyle}: IconComponentType): ReactElement | ReactElement[] {
+  return (
+    <IconWrapper wrapperCustomStyle={wrapperCustomStyle}>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        fill="none" 
+        viewBox="0 0 24 24"
+        width={width ||  "100%"}
+        height={height || "100%"}
+      >
+        <g 
+          stroke={stroke || "#000" }
+          strokeLinejoin="round" 
+          strokeWidth="1.5"
+        >
+          <path d="M19 9v8c0 1.886 0 2.828-.586 3.414C17.828 21 16.886 21 15 21H9c-1.886 0-2.828 0-3.414-.586C5 19.828 5 18.886 5 17V9"></path>
+          <path
+            strokeLinecap="round"
+            d="M3 11l4.5-4 3.171-2.819a2 2 0 012.658 0L16.5 7l4.5 4M10 21v-4a2 2 0 012-2v0a2 2 0 012 2v4"
+          ></path>
+        </g>
+      </svg>
+    </IconWrapper>
+  );
+}
 
 // Logo
 export function LogoIcon({width, height, wrapperCustomStyle}: IconComponentType): ReactElement | ReactElement[] {
