f777870c71aab691df8baf0b9ecd360bd59b2a51
Agathius <p.akos1148@gmail.com>
2023-10-26 18:04:11 +0200

    updated sidemenu layout, remved unused style

--- src/components/ProductSidemenu/ProductSidemenu.tsx
+++ src/components/ProductSidemenu/ProductSidemenu.tsx
@@ -1,7 +1,8 @@
 // Product page menu that holds product categories and additional menu elements
-import './ProductSidemenu.css'
 import {ProductCategories} from './ProductCategories'
 import {ProductsSidemenuPropsType} from '../../types/productSidemenuTypes'
+import ProductFilters from './ProductFilters'
+import './ProductSidemenu.css'
 
 // COMPONENT
 const ProductSidemenu = ({activeCategory}: ProductsSidemenuPropsType) => {
@@ -9,6 +10,7 @@ const ProductSidemenu = ({activeCategory}: ProductsSidemenuPropsType) => {
   return (
     <div className='product-sidemenu'>
       <ProductCategories activeCategory={activeCategory}/>
+      <ProductFilters/>
     </div>
   )
 }
