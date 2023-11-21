25345f3f85398114db5b416649686f8dfebcb654
Agathius <p.akos1148@gmail.com>
2023-11-21 23:53:22 +0100

    connected price filter and product list display

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,26 +1,33 @@
 // Component to hold the product items and related pagination
-import {useEffect, ReactElement} from 'react'
-import './ProductPage.css'
-import {ProductItemType} from '../types/productsProviderTypes'
+import { useEffect, ReactElement } from 'react'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { ProductItemType } from '../types/productsProviderTypes'
 import Pagination from './Pagination'
 import ProductList from './ProductList'
-import {useNavigate, useLocation} from 'react-router-dom'
 import ProductSidemenu from './ProductSidemenu/ProductSidemenu'
 import useProducts from '../hooks/useProducts'
+import './ProductPage.css'
 
+// CONSTANT
 const CONSTANT = {
   HEADER_1_BROWSE_GOODIES: 'Browse Goodies',
   HEADER_1_NO_PRODUCTS: 'No Available Products',
-  HEADER_2_NEW_PRODUCTS_SOON: 'New products are coming soon...'
+  HEADER_2_NEW_PRODUCTS_SOON: 'New products are coming soon...',
+  PRODUCTS_FILTERED_RESULT: 'Filtered products',
+  PRODUCTS_FILTERED_NO_RESULT: 'No results, check out our stock'
 }
 
-// TYPE
+// TYPES
 type ProductPagePropsType = {
   productData: ProductItemType[] | undefined
 }
+type IndexRangePriceType = {
+  start: number,
+  end: number
+}
 
 // COMPONENT
-const ProductPage = ({productData}: ProductPagePropsType) => {
+const ProductPage = ({ productData }: ProductPagePropsType) => {
   // ROUTE
   const navigate = useNavigate();
   // const {category, page} = useParams();
@@ -28,62 +35,107 @@ const ProductPage = ({productData}: ProductPagePropsType) => {
   const activeCategory = location.pathname.split('/').filter(segment => segment !== '').slice(0, -1).join('/');
 
   // CONTEXT
-  const {activePage} = useProducts();
-  const pageNumber = activePage || 1
+  const { isFilteringProduct, activePage, categoryProducts, categoryProductsFiltered, dispatch, REDUCER_ACTIONS_PRODUCT } = useProducts();
+  
+  // VALUE
+  const pageNumber = activePage || 1;
 
   // EFFECTS
+  // Store last visited page in 
   useEffect(() => {
     sessionStorage.setItem('lastVisitedPage', 'products');
   }, [])
 
+  // Navigate to route based on active route param
   useEffect(() => {
-    const activeRoute = activeCategory === 'search' ? '/search/1' : `/${activeCategory}/${pageNumber}`
+    const activeRoute = activeCategory === 'search' ? '/search/1' : `/${ activeCategory }/${ pageNumber }`
     navigate(activeRoute, { replace: true });
-  }, [activeCategory, navigate, pageNumber]);
+  }, [activeCategory, navigate, pageNumber])
 
-  // Filter out productsData based on active category
-  const categoryProductData = productData?.filter(product => {
-    return product.category === activeCategory;
-  });
+  // Filter and store active category products
+  useEffect(() => {
+    const categoryProductData = productData?.filter((product: ProductItemType) => {
+      if(activeCategory === 'all' || activeCategory === 'search') {
+        return product;
+      } else {
+        return product.category === activeCategory;
+      }
+    }); 
 
-  const filteredProductData = activeCategory === 'all' || activeCategory === 'search'  ? productData : categoryProductData
+    // update store with filtered products
+    dispatch({ type: REDUCER_ACTIONS_PRODUCT.UPDATE_CATEGORY_PRODUCTS, payload: { categoryProducts: categoryProductData } });
+  }, [REDUCER_ACTIONS_PRODUCT.UPDATE_CATEGORY_PRODUCTS, activeCategory, dispatch, productData])
+  
 
-  // CONSTANT VALUES
-  const itemsPerPage = 10;
+  // Apply pagination to product data:
+  const paginateData = (productsData: ProductItemType[] | undefined, indexRange: IndexRangePriceType): ProductItemType[] => {
+    return productsData?.slice(indexRange.start, indexRange.end) || [];
+  } 
 
-  // CALCULATED VALUES
-  const displayedProductData: ProductItemType[] = filteredProductData || []
-  const totalPages = Math.ceil(displayedProductData?.length / itemsPerPage);
-  const startIndex: number = (pageNumber - 1) * itemsPerPage;
-  const endIndex: number = startIndex + itemsPerPage;
+  // Displayed product list
+  const productList = (): ReactElement | ReactElement[] => {
+    let headerContent;
+    let productListContent: ProductItemType[] = [];
 
-  const paginatedProducts: ProductItemType[] = filteredProductData?.slice(startIndex, endIndex) || [];
+    const productCategories: string[] = ['all', 'mobile','processor', 'ram', 'videocard']; // TODO: get keys dynamically
+ 
+    const isCategoryProductsAvailable: boolean = !!categoryProducts?.length;
+    const isFilteredCategoryProductsAvailable: boolean = !!categoryProductsFiltered?.length;
+    const itemsPerPage = 10;
+    const indexRange: IndexRangePriceType = {
+      start: (pageNumber - 1) * itemsPerPage,
+      end: (pageNumber - 1) * itemsPerPage + itemsPerPage
+    }
 
-  // empty products list (fetch ok)
-  const productEmptyList: ReactElement | ReactElement[] = (
-    <div className="product-page__empty-list">
-      <h1 className='product-page__header--1'>{CONSTANT.HEADER_1_NO_PRODUCTS}</h1>
-      <h2 className='product-page__header--2'>{CONSTANT.HEADER_2_NEW_PRODUCTS_SOON}</h2>
-    </div>
-  )
+    if(productData === undefined || !productData.length) {
+      headerContent = CONSTANT.HEADER_1_NO_PRODUCTS;
+      productListContent;
+    }
+    // category products
+    else if(productCategories.includes(activeCategory)) {
+      // filter result
+      if(isFilteredCategoryProductsAvailable) {
+        headerContent = CONSTANT.PRODUCTS_FILTERED_RESULT;
+        productListContent = categoryProductsFiltered || [];
+      // filter no result
+      } else if(isCategoryProductsAvailable && isFilteringProduct) {
+        headerContent = CONSTANT.PRODUCTS_FILTERED_NO_RESULT;
+        productListContent = categoryProducts  || [];
+      // default
+      } else if(isCategoryProductsAvailable) {
+        headerContent = CONSTANT.HEADER_1_BROWSE_GOODIES;
+        productListContent = categoryProducts  || [];
+      } 
+      // if no product in the category
+      else if(!isCategoryProductsAvailable) {
+        headerContent = 'No product in this category';
+        productListContent = [];
+      }
+    } 
 
-  // products list
-  const productList: ReactElement | ReactElement[] = (
-    <div className='product-page__content'>
-      <h1 className='product-page__header--1'>{CONSTANT.HEADER_1_BROWSE_GOODIES}</h1>
-      <ProductList productsData={paginatedProducts}/>
-      <Pagination totalPages={totalPages}/>
-    </div>
-  )
+    // pagination
+    const totalPages = Math.ceil(productListContent?.length / itemsPerPage);
+    const paginatedProducts: ProductItemType[] = paginateData(productListContent, indexRange);
 
+    // displayed products
+    return (
+      <div className='product-page__content'>
+        <h1 className='product-page__header--1'>{ headerContent }</h1>
+        <h3 className='product-page__header--2'> { 'h3 test' } </h3>
+        <ProductList productsData={ paginatedProducts || [] }/>
+        { !!productListContent.length && <Pagination totalPages={ totalPages }/> }
+      </div>
+    )
+  }
+      
   return (
     <main className='main main__product-page'>
       <div className="product-page__wrapper">
         <ProductSidemenu activeCategory={activeCategory}/>
-        {filteredProductData?.length ? productList : productEmptyList}
+        { productList() }
       </div>
     </main>
   )
 }
 
-export default ProductPage
+export default ProductPage;
