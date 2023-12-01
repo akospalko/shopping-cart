95abfe2039941aa0a1e0219cb29fcf9632504fc1
Agathius <p.akos1148@gmail.com>
2023-12-02 00:02:36 +0100

    Added focus navigation logic and style

--- src/components/UI/ProductRating.tsx
+++ src/components/UI/ProductRating.tsx
@@ -1,13 +1,13 @@
 // UI element to set/remove, display product rating
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import { HeartIcon } from "../SVGComponents";
 import "./ProductRating.css";
 
 // TYPE
 type ProductRatingPropsType = {
-  readOnly: boolean,
-  productRating: number,
-  displaydRatingElements: number
+  readOnly?: boolean,
+  productRating?: number,
+  displaydRatingElements?: number
 }
 
 const ProductRating = ({ readOnly = false, productRating = 0, displaydRatingElements = 5 }: ProductRatingPropsType ) => {
@@ -21,10 +21,19 @@ const ProductRating = ({ readOnly = false, productRating = 0, displaydRatingElem
   const [rating, setRating] = useState<number>(initialRating);
   const [hover, setHover] = useState<number | null>(null);
 
-  const handleClick = (selectedRating: number) => {
+  // HANDLER
+  const ElementClickHandler = (selectedRating: number) => {
     if (!readOnly) {
       // If the selected rating is the same as the current rating, reset to 0 (remove rating)
       const newRating = selectedRating === rating ? 0 : selectedRating;
+      setRating(newRating);
+    }
+  };
+
+  const KeyDownHandler = (event: KeyboardEvent<HTMLDivElement>, currentRating: number) => {
+    if (!readOnly && (event.key === "Enter")) {
+      // Space or Enter key
+      const newRating = currentRating === rating ? 0 : currentRating;
       setRating(newRating);
     }
   };
@@ -34,21 +43,27 @@ const ProductRating = ({ readOnly = false, productRating = 0, displaydRatingElem
       { [...Array(displaydRatingElements)].map((_, index: number) => {
         const currentRating = index + 1;
         return (
-          <label key={ index } className="product-label">
+          <label 
+            className="product-label"
+            key={ index }
+          >
             <input
+              className="product-radio"
               type="radio"
               name="rating"
               value={ currentRating }
               checked={ currentRating === rating }
               onChange={ () => {} } // Prevent console warnings about uncontrolled/controlled inputs
               readOnly={ readOnly }
-              className="product-radio"
+              tabIndex={ readOnly ? -1 : 0 }
             />
             <div
-              onClick={ () => handleClick(currentRating) }
+              className="product-rating"
+              onClick={ () => ElementClickHandler(currentRating) }
+              onKeyDown={ (event: KeyboardEvent<HTMLDivElement>) => KeyDownHandler(event, currentRating) }
               onMouseEnter={ () => !readOnly && setHover(currentRating) }
               onMouseLeave={ () => !readOnly && setHover(null) }
-              className="product-rating"
+              tabIndex={ readOnly ? -1 : 0 }
             >
               <HeartIcon
                 width={ iconSize }
