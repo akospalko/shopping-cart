440515205f3e7c0dddfff6b905c0a45161d00c16
Agathius <p.akos1148@gmail.com>
2023-12-03 16:25:55 +0100

    removed unused parts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 // import Footer from "./components/Footer";
 import CartPage from "./components/CartPage";
-import useProducts from "./hooks/useProducts";
 import ErrorPage from "./components/ErrorPage";
 import ProductPage from "./components/ProductPage";
 import ProductView from "./components/ProductView";
@@ -20,20 +19,17 @@ function App() {
   // STATE
   const [viewCart, setViewCart] = useState<boolean>(false);
 
-  // CONTEXT
-  const { products, filteredProducts } = useProducts();
-
   return (
     <div className="page">
       <Header viewCart={ viewCart } setViewCart={ setViewCart }/>
       <Routes>
         <Route path="/cart" element={ <CartPage/> }/>
-        <Route path="/:category/:page?" element={ <ProductPage productData={ products }/> }/>
+        <Route path="/:category/:page?" element={ <ProductPage/> }/>
         <Route path="/:category/product/:product/about" element={ <ProductView activeTab={ CONSTANT.ABOUT }/> } />  
         <Route path="/:category/product/:product/characteristics" element={ <ProductView activeTab={ CONSTANT.CHARACTERISTICS }/> }/>  
-        <Route path="/search/:page?" element={ <ProductSearchPage productData={ filteredProducts }/> }/>  
+        {/* TODO: <Route path="/:category/product/:product/reviews" element={ <ProductView activeTab={ CONSTANT.REVIEWS }/> }/>   */}
+        <Route path="/search/:page?" element={ <ProductSearchPage/> }/>  
         <Route path="/" element={ <Navigate to="/all/1"/> }/>
-        
         {/* <Route path="/*" element={ <Navigate to="/error"/> }/> */}
         <Route path="/error" element={ <ErrorPage content={ { title: "test", subtitle: "test" } }/> }/>
       </Routes>
