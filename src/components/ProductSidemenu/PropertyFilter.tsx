266fff2bc5621d7025fb92a1de052e0d170ca9fa
Agathius <p.akos1148@gmail.com>
2023-12-10 23:55:05 +0100

    refactored property filter, added stylesheet with basic styling.

--- src/components/ProductSidemenu/PropertyFilter.tsx
+++ src/components/ProductSidemenu/PropertyFilter.tsx
@@ -1,49 +1,47 @@
+// TODO: ADD toggler to groups: 
 // Sidebar component that displays grouped checkbox logic for product property filtering 
 import Checkbox from "../UI/Checkbox";
-
-const GroupedList = ({ groupedData, handleCheckboxChange }) => {
-  return (
-    <div>
-      {Object.keys(groupedData).map((key) => {
-        const groupCheckboxArray = groupedData[key];
-        return (
-          <div key={ key }>
-            <h3>{ key }</h3>
-            {groupCheckboxArray?.map((checkbox, i) => (
-              <div key={i}>
-                <Checkbox
-                  label={checkbox.filter}
-                  isChecked={checkbox.isChecked}
-                  onChange={(e) => handleCheckboxChange(e, checkbox.filter, key)}
-                />
-                <span> {`(${checkbox.count})`} </span>
-              </div>
-            ))}
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+import useFilter from "../../hooks/useFilter";
 
 // Product Property Filter
-const PropertyFilter = ({ propertyFilterGroups, setPropertyFilterGroups }) => {
+const PropertyFilter = () => {
+  // CONTEXT
+  const { filterOptions, setFilterOptions } = useFilter();
+
   // HANDLER
   const handleCheckboxChange = (e, checkboxName, groupName) => {
-    const updatedValue = JSON.parse(JSON.stringify(propertyFilterGroups));
+    console.log(checkboxName, groupName)
+
+    const updatedValue = JSON.parse(JSON.stringify(filterOptions));
     const checkboxGroup = updatedValue[groupName];
     // Find the checkbox in the group and update its isChecked property
     const updatedCheckbox = checkboxGroup.find((checkbox) => checkbox.filter === checkboxName);
     if (updatedCheckbox) {
       updatedCheckbox.isChecked = e.target.checked;
     }
-    setPropertyFilterGroups(updatedValue);
+    setFilterOptions(updatedValue);
   };
 
-    
   return (
     <div className='propety-filter'>
-      <GroupedList groupedData={ propertyFilterGroups } handleCheckboxChange={ handleCheckboxChange } />
+      { Object.keys(filterOptions)?.map((key) => {
+        const groupCheckboxArray = filterOptions[key];
+        return (
+          <div key={ key }>
+            <h3>{ key }</h3>
+            {groupCheckboxArray?.map((checkbox, i) => (
+              <div key={ i }>
+                <Checkbox
+                  label={ checkbox?.displayedFilterName ?? checkbox.filter }
+                  isChecked={ checkbox.isChecked }
+                  onChange={ (e) => handleCheckboxChange(e, checkbox.filter, key) }
+                />
+                <span> { `(${checkbox.count})` } </span>
+              </div>
+            )) }
+          </div>
+        );
+      }) }
     </div>
   )
 }
