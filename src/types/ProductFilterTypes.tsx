8b25adf48f28bd74946bc67fd9b7d48ccf019842
Agathius <p.akos1148@gmail.com>
2023-12-13 11:32:49 +0100

    updated types: added new filter related types and product property types

--- src/types/ProductFilterTypes.tsx
+++ src/types/ProductFilterTypes.tsx
@@ -1,5 +1,6 @@
 // Gathered types used for product filtering 
 import { ProductItemType } from './productsProviderTypes';
+import { GroupKeysType } from './productsProviderTypes';
 
 // FILTER BY PRICE
 // Shared
@@ -7,12 +8,9 @@ export type PriceValueType = { minValue: string, maxValue: string }
 
 // PriceFilter
 export type PriceFilterPropsType = { categoryProducts: ProductItemType[] }
-export type PriceRangeAndValueType = [number, number];
+export type PriceRangeAndValueType = [number, number]; 
 
 export type SliderPropsTypes = {
-  priceRange: PriceRangeAndValueType,
-  priceValues: PriceRangeAndValueType,
-  onChange: React.Dispatch<React.SetStateAction<PriceRangeAndValueType>>,
   disabled: boolean
 }
 
@@ -32,3 +30,46 @@ export type PriceTextInputPropsType = {
   setInputValue: React.Dispatch<React.SetStateAction<PriceValueType>>,
   disabled: boolean
 }
+
+
+// Filter group 
+export type FilterGroupRangeType = {
+  unit: string;
+  ranges: ([number, number] | [number])[];
+}
+
+export type FilterGroupType = {
+  group: GroupKeysType
+  range?: FilterGroupRangeType
+};
+
+export type FilterGroupPropertiesType = {
+  processor: FilterGroupType[];
+  videoCard: FilterGroupType[];
+  ram: FilterGroupType[];
+  mobile: FilterGroupType[];
+};
+
+// Filter options
+export type DefaultFilterOptionType = {
+  filter: string,
+  count: number,
+  isChecked: boolean
+};
+
+export type RangeFilterMinMaxType = [number, number] | [number];
+export type RangeFilterOptionType = {
+  filter: number,
+  displayedFilterName: string,
+  count: number,
+  isChecked: boolean,
+  range: string,
+  minMaxRange: RangeFilterMinMaxType
+};
+
+export type FilterOptionsType = {
+  [ key in GroupKeysType ]: (DefaultFilterOptionType | RangeFilterOptionType)[]
+}
+
+
+
--- src/types/productsProviderTypes.tsx
+++ src/types/productsProviderTypes.tsx
@@ -1,13 +1,14 @@
 import { ReactElement } from "react";
 import { useProductContext } from "../context/ProductsProvider";
 import { SORT_OPTION_VALUE } from "../utility/constants";
+
 // TYPES
 // Product item properties type based on different category
 export type ProductProcessorPropertyType = {
   ['brand']: string,
   ['model']: string,
   ['architecture']: string,
-  ['cores']: string | number,
+  ['cores']: string,
   ['threads']: string,
   ['clock speed']: string,
   ['cache']: string,
@@ -68,7 +69,31 @@ export type ProductReviewPropertyType = {
   ["review"]: string
 } 
 
-// Product type
+// Product property type
+export type ProductMergedPropertiesType = ProductProcessorPropertyType | ProductRamPropertyType | 
+ProductVideoCardPropertyType | 
+ProductMobilePropertyType;
+
+// Product property type string literal keys
+// define the mapped type to extract string keys
+export type ExtractStringKeys<T> = {
+  [K in keyof T]: T[K] extends string ? K : never;
+};
+
+// use the mapped type to extract string keys from each property type
+export type VideoCardKeys = ExtractStringKeys<ProductVideoCardPropertyType>;
+export type MobileKeys = ExtractStringKeys<ProductMobilePropertyType>;
+export type ProcessorKeys = ExtractStringKeys<ProductProcessorPropertyType>;
+export type RamKeys = ExtractStringKeys<ProductRamPropertyType>;
+
+// combine all extracted keys into a union
+export type GroupKeysType =
+  | VideoCardKeys[keyof VideoCardKeys]
+  | MobileKeys[keyof MobileKeys]
+  | ProcessorKeys[keyof ProcessorKeys]
+  | RamKeys[keyof RamKeys];
+
+// Product Item
 export type ProductItemType = {
   sku: string,
   name: string,
@@ -81,15 +106,7 @@ export type ProductItemType = {
   retailer: string,
   review: ProductReviewPropertyType[],
   calculatedRatingAvg: number,
-  properties: ProductProcessorPropertyType | ProductRamPropertyType | 
-  ProductVideoCardPropertyType | 
-  ProductMobilePropertyType | ProductItemType | undefined
-}
-
-// ----------REDUCER----------
-export type ReducerAction = {
-  type: string,
-  payload?: ProductStateType
+  properties: ProductMergedPropertiesType
 }
 
 // ----------PRODUCT CONTEXT LOGIC----------
@@ -101,7 +118,13 @@ export type ProductStateType = {
   searchTerm?: string,
   isFilteringProduct?: boolean,
   searchStatus?: string,
-  activeSortOption?: SORT_OPTION_VALUE
+  activeSortOption?: SORT_OPTION_VALUE,
+}
+
+// ----------REDUCER----------
+export type ReducerAction = {
+  type: string,
+  payload?: ProductStateType
 }
 
 // ----------CREATE CONTEXT----------
