b8c73e230c5a63db05af6fa3a41d9eb583b7a62e
Agathius <p.akos1148@gmail.com>
2023-12-14 23:43:18 +0100

    Added new svg icons: menu dots and filter icon

--- src/components/SVGComponents.tsx
+++ src/components/SVGComponents.tsx
@@ -299,3 +299,49 @@ export function QuestionMarkIcon({ width, height, fill, fill2 }: IconComponentTy
     </IconWrapper>
   );
 }
+
+// Menu
+export function MenuIcon({ width, height, stroke, strokeWidth, wrapperCustomStyle }: IconComponentType): ReactElement | ReactElement[] {
+  return (
+    <IconWrapper wrapperCustomStyle={ wrapperCustomStyle }>
+      <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width={ width ||  "100%" }
+      height={ height || "100%" }
+      fill="none" 
+      viewBox="0 0 24 24">
+        <g
+          stroke={ stroke || "#000" }
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={ strokeWidth || "2"}
+        >
+          <path d="M19 13a1 1 0 100-2 1 1 0 000 2zM12 13a1 1 0 100-2 1 1 0 000 2zM5 13a1 1 0 100-2 1 1 0 000 2z"></path>
+        </g>
+      </svg>
+    </IconWrapper>
+  );
+}
+
+// Menu
+export function FilterIcon({ width, height, stroke }: IconComponentType): ReactElement | ReactElement[] {
+  return (
+    <IconWrapper>
+     <svg 
+      width={ width ||  "100%" }
+      height={ height || "100%" }
+      xmlns="http://www.w3.org/2000/svg" 
+      fill="none" 
+      viewBox="0 0 24 24"
+     >
+      <path
+        stroke={ stroke || "#000"}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M3 4.6c0-.56 0-.84.109-1.054a1 1 0 01.437-.437C3.76 3 4.04 3 4.6 3h14.8c.56 0 .84 0 1.054.109a1 1 0 01.437.437C21 3.76 21 4.04 21 4.6v1.737c0 .245 0 .367-.028.482a.998.998 0 01-.12.29c-.061.1-.148.187-.32.36l-6.063 6.062c-.173.173-.26.26-.322.36a.998.998 0 00-.12.29c-.027.115-.027.237-.027.482V17l-4 4v-6.337c0-.245 0-.367-.028-.482a1 1 0 00-.12-.29c-.061-.1-.148-.187-.32-.36L3.468 7.47c-.173-.173-.26-.26-.322-.36a1 1 0 01-.12-.29C3 6.704 3 6.582 3 6.337V4.6z"
+      ></path>
+    </svg>
+    </IconWrapper>
+  );
+}
