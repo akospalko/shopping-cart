901064a616cdd7b065a804d10bb3b54ed17d4a1e
Agathius <p.akos1148@gmail.com>
2023-12-22 23:26:34 +0100

    added new constants

--- src/utility/constants.tsx
+++ src/utility/constants.tsx
@@ -1,9 +1,12 @@
 // Enums Constants and magic numbers, initializers
-
-
+import { ModalTypes } from "../types/navigationMenuTypes";
 // 
 export const PRODUCT_CATEGORY = {
-  ALL: "all"
+  ALL: "all",
+  PROCESSOR: "processor",
+  MOBILE: "MOBILE",
+  VIDEO_CARD: "VIDEO_CARD",
+  RAM: "RAM"
 }
 // PriceFilter.tsx
 export const priceFilterStateInitializer = {
@@ -38,18 +41,31 @@ export enum SORT_ORDER {
   DESCENDING = "DESCENDING"
 }
 
-// ???
+// ProductView.tsx, ...
 export const enum PRODUCT_VIEW_TAB {
-  ABOUT = "about",
-  CHARACTERSISTICS = "charactersistics",
-  REVIEW = "review"
+  ABOUT = "ABOUT",
+  CHARACTERISTICS = "CHARACTERSISTICS",
+  REVIEW = "REVIEW"
 } 
 
 // NavigationMenu.tsx
 export const NAVIGATION_MENU_ITEMS_ACTION = {
   MAIN_MENU: "MAIN_MENU",
   PRODUCT_CATEGORY: "PRODUCT_CATEGORY",
+  MAIN_BAR: "MAIN_BAR"
 } as const;
+
+
+// NavigationMenuProvider.tsx
+export const MODAL_TOGGLE_STATE_INITIALIZER: ModalTypes = {
+  MAIN_MENU: false,
+  FILTER_MENU: false
+}
+// NavigationMenuProvider.tsx
+export const enum MODAL_TOGGLE_KEY {
+  MAIN_MENU = "MAIN_MENU",
+  FILTER_MENU = "FILTER_MENU"
+} 
 
 // Misc
 export const CONSTANT = { 
