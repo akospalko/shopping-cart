0ea87c813b4eb09a0600226415ddbb310ba34a43
Agathius <p.akos1148@gmail.com>
2023-12-24 02:03:57 +0100

    Added Layout to Each page; created new variables for header , z index values, removed footer styling from index.css;  applied context provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { CartProvider } from './context/CartProvider.tsx'
-import { ProductsProvider } from './context/ProductsProvider.tsx'
-import { BrowserRouter } from "react-router-dom"
-import { FilterProvider } from './context/FilterProvider.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { CartProvider } from "./context/CartProvider.tsx";
+import { ProductsProvider } from "./context/ProductsProvider.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { FilterProvider } from "./context/FilterProvider.tsx";
+import { NavigationMenuProvider } from "./context/NavigationMenuProvider.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ProductsProvider>
         <FilterProvider>
           <CartProvider>
-            <App/>
+            <NavigationMenuProvider>
+              <App/>
+            </NavigationMenuProvider>
           </CartProvider>
         </FilterProvider>
       </ProductsProvider>
