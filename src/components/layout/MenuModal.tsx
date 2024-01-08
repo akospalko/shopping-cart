0b4f0667d12a77fcf24bbe03efdb310a428cc59e
Agathius <p.akos1148@gmail.com>
2024-01-09 00:26:24 +0100

    Added close modal on modal backdrop click

--- src/components/layout/MenuModal.tsx
+++ src/components/layout/MenuModal.tsx
@@ -1,9 +1,14 @@
 // Reusable menu modal; used for: filter and navigation menus 
-import { ReactNode, ReactElement, ForwardedRef, forwardRef } from "react";
+import { 
+  ReactNode, 
+  ReactElement, 
+  ForwardedRef, 
+  forwardRef
+} from "react";
 import { RemoveIcon } from "../SVGComponents";
 import useNavigationMenu from "../../hooks/useNavigationMenu";
-import { MODAL_TOGGLE_KEY } from "../../utility/constants";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import { MODAL_TOGGLE_KEY } from "../../utility/constants";
 import "./MenuModal.css";
 
 // TYPES
@@ -17,14 +22,15 @@ type MenuModalButtonStyleType = {
   button: string
 }
 
+// COMPONENT
 const MenuModal = forwardRef(({ children, toggleModalKey }: MenuModalPropsType, ref: ForwardedRef<HTMLDivElement>) => {
   // CONTEXT
   const { toggleModal } = useNavigationMenu();
-  
+
   // HOOKS
   const isVerySmallScreen: boolean = useMediaQuery("(max-width: 359px)");
   const isSmallToLargeScreen: boolean = useMediaQuery("(min-width: 360px)");
-  
+
   // STYLE
   const iconSize: string = "20px"; 
   const iconColor: string = "var(--color-5)";
@@ -43,7 +49,7 @@ const MenuModal = forwardRef(({ children, toggleModalKey }: MenuModalPropsType, 
     <div className={ customStyle.wrapper || "" }>
       <button 
         className={ `button--close-menu-modal ${ customStyle.button }` }
-        onClick={ () => toggleModal(toggleModalKey, true) }
+        onClick={ () => { toggleModal(toggleModalKey, true) } }
       > 
         <RemoveIcon 
           width={ iconSize }
@@ -71,9 +77,13 @@ const MenuModal = forwardRef(({ children, toggleModalKey }: MenuModalPropsType, 
 
   // Small to large
   const smallToLargeScreenLayout: ReactElement = (
-    <div className="menu-modal__backdrop">
+    <div 
+      className="menu-modal__backdrop"
+      onClick={ () => { toggleModal(toggleModalKey, true) } }
+    >
       <div 
         className="menu-modal__modal"
+        onClick={ e => e.stopPropagation() }
         ref={ ref }
       >
         <div className="menu-modal__modal-content">
